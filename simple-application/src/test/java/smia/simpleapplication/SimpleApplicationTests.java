aed280295f358449e55b5d7463605f9c5b082f92
boldfaced7 <boldfaced753@gmail.com>
2023-04-29 13:49:02 +0900

    스프링 부트 스타터를 이용한 스프링 프로젝트 생성

---
+++ simple-application/src/test/java/smia/simpleapplication/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package smia.simpleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
