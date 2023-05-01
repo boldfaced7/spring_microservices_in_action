346c695f12ef14038da95ef2255c57a33b224df8
준모 강 <boldfaced7@hanyang.ac.kr>
2023-05-01 14:51:04 +0900

    License 모델 클래스에 링크를 주가할 수 있게 하는 RepresentationModel<License>를 상속

--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -3,9 +3,11 @@ package com.optimagrowth.license.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter @Setter @ToString
-public class License {
+// License 모델 클래스에 링크를 주가할 수 있게 하는 RepresentationModel<License>를 추가
+public class License extends RepresentationModel<License> {
     private int id;
     private String licenseId;
     private String description;
