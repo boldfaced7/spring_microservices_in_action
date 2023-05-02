338fe32f3b40b406ca45599d3eed9e291dc65a27
준모 강 <boldfaced7@hanyang.ac.kr>
2023-05-02 19:41:28 +0900

    Spring Data JPA 사용을 위한 어노테이션 작성

--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -5,13 +5,31 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Getter @Setter @ToString
+@Entity
+@Table(name = "licenses")
 // License 모델 클래스에 링크를 주가할 수 있게 하는 RepresentationModel<License>를 추가
 public class License extends RepresentationModel<License> {
-    private int id;
+    @Id
+    @Column(name = "license_id", nullable = false)
     private String licenseId;
     private String description;
+    @Column(name = "organization_id", nullable = false)
     private String organizationId;
+    @Column(name = "product_name", nullable = false)
     private String productName;
+    @Column(name = "licenseType", nullable = false)
     private String licenseType;
+    @Column(name = "comment")
+    private String comment;
+
+    public License withComment(String comment) {
+        this.setComment(comment);
+        return this;
+    }
 }
