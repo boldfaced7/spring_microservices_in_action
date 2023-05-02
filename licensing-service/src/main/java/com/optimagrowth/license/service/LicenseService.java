8b4bf151f58d6a4fb53b0a6d5536712043d0c17c
준모 강 <boldfaced7@hanyang.ac.kr>
2023-05-02 19:43:36 +0900

    LicenseRepository 사용을 위한 수정

--- licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,55 +1,45 @@
 package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.repository.LicenseRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
-import java.util.Locale;
-import java.util.Random;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class LicenseService {
     @Autowired
-    MessageSource messages;
-    public License getLicense(String licenseId, String organizationId) {
-        License license = new License();
-        license.setId(new Random().nextInt(1000));
-        license.setLicenseId(licenseId);
-        license.setOrganizationId(organizationId);
-        license.setDescription("Software product");
-        license.setProductName("Ostock");
-        license.setLicenseType("full");
+    MessageSource messageSource;
+
+    @Autowired
+    private LicenseRepository licenseRepository;
+
+    public Optional<License> getLicense(String licenseId, String organizationId) {
+        Optional<License> license = licenseRepository.
+                findByOrganizationIdAndLicenseId(organizationId, licenseId);
         return license;
     }
-    // 메서드 매개변수로 로케일을 전달 받음
-    public String createLicense(License license, String organizationId, Locale locale) {
-        String responseMessage = null;
-        if (!ObjectUtils.isEmpty(license)) {
-            license.setOrganizationId(organizationId);
-            // 특정 메시지를 조회하기 위해 전달된 로케일로 설정
-            responseMessage = String.format(messages.getMessage(
-                    "license.create.message", null, locale),
-                    license.toString());
-        }
-        return responseMessage;
+    public License createLicense(License license) {
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
+        return license;
     }
-    public String updateLicense(License license, String organizationId) {
-        String responseMessage = null;
-        if (!ObjectUtils.isEmpty(license)) {
-            license.setOrganizationId(organizationId);
-            // 특정 메시지를 조회하기 위해 NULL 로케일을 전달해, 디폴트 로케일을 사용
-            responseMessage = String.format(messages.getMessage(
-                    "license.update.message", null, null),
-                    license.toString());
-        }
-        return responseMessage;
+    public License updateLicense(License license) {
+        licenseRepository.save(license);
+        return license;
     }
-    public String deleteLicense(String licenseId, String organizationId) {
+    public String deleteLicense(String licenseId) {
         String responseMessage = null;
-        responseMessage = String.format("Deleting license with id %s for the " +
-                "organization %s", licenseId, organizationId);
+        License license = new License();
+        license.setLicenseId(licenseId);
+        licenseRepository.delete(license);
+
+        responseMessage = String.format(messageSource.getMessage(
+                "license.delete.message %s", null, null), licenseId);
         return responseMessage;
     }
 }
